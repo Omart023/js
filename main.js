b024debaee6f89c21fdb40c30b368b05fa9b598a
omart23 <obenitez2316@gmail.com>
2023-08-08 23:13:50 -0300

    Primera entrega

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 //Ingreso al centro de ayuda de Movistar
 
-function problemas_linea() {
-    alert("Por favor reinicie el equipo");
+function problemas_linea1() {
+    alert("Por favor reinicie el equipo,si el problema persiste pida hablar con asesor");
+}
+
+function problemas_linea2() {
+    alert("Verifique si cuenta con paquete de datos vigente");
+}
+
+function problemas_linea3() {
+    alert("Usted tiene un saldo deudo de $3500, por favor abonelo para que se le restaure la linea, muchas gracias");
 }
 
 
 const usuario = "Alberto23"
-const contraseña = "1234"
+const contrasenia = "1234"
 const opcion1 = 1
 const opcion2 = 2
 const opcion3 = 3
@@ -25,7 +33,7 @@ do{
     let usuar = prompt("Ingrese su usuario")
     let contra = prompt("Ingrese contraseña.")
 
-    if(usuario == usuar && contraseña == contra){
+    if(usuario == usuar && contrasenia == contra){
     alert("Ingreso exitoso, Alberto23!")
     alert("Indique a traves de los numeros la opcion deseada para poder Ayudarlo")
 
@@ -33,13 +41,13 @@ do{
     
     let opcion = prompt("1-Problemas con la linea\n2-Problemas con internet\n3-Estado de cuenta")
     if(opcion==opcion1){
-        problemas_linea();
+        problemas_linea1();
     }
     if(opcion==opcion2){
-        alert("Verifique si cuenta con paquete de datos vigente")
+        problemas_linea2();
     }
     if(opcion==opcion3){
-        alert("Usted tiene un saldo deudo de $3500, por favor abonelo para que se le restaure la linea, muchas gracias")
+        problemas_linea3()
     }
 
     break
